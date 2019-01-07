0567286d1c9ab503261ef68ccf204ca8ec1ab632
Elle Nolan <ellemnolan@gmail.com>
2019-01-06 22:13:18 -0500

    Starting to combine functionality and interface

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const tmi = require("tmi.js");
+const q = require("./match-queue")
+const http = require("http");
 
-
+// Options 
 const options = {
     options: {
         debug: true
@@ -19,19 +21,6 @@ const options = {
 const channel = "Professor_L_Tetris";
 const client = new tmi.client(options);
 
-
-client.connect();
-
-client.on("connected", (address, port) => {
-    client.action(channel, "LsQ Bot connected. (( Source code: https://github.com/professor-l/lsq-bot. ))");
-});
-
-client.on("chat", (channel, user, message, self) => {
-    if (message == "!qhelp" || message == "!lsqhelp")
-        printHelp();
-});
-
-
 const helpString = `
                 Match commands: 
                 !challenge,
@@ -43,6 +32,53 @@ const helpString = `
                 !lsqhelp (or !qhelp).
                 See https://github.com/professor-l/lsq-bot#readme for more.
                 `;
-function printHelp() {
-    client.action(channel, helpString);
-}
+
+let GlobalQueue = new MatchQueue();
+
+
+client.connect();
+
+client.on("connected", (address, port) => {
+    client.action(channel, "LsQ Bot connected. (( Source code: https://github.com/professor-l/lsq-bot. ))");
+});
+
+client.on("chat", (channel, user, message, self) => {
+    if (message == "!qhelp" || message == "!lsqhelp")
+        client.action(channel, helpString);
+    
+    if (message.substring(0, 11 == "!challenge ")) {
+        http.get("http://tmi.twitch.tv/group/user/{}/chatters".format(channel), (response) => {
+
+            let data = "";
+            response.on("data", (chunk) => {
+                data += chunk;
+            });
+
+            response.on("end", () => {
+                let d = JSON.parse(data).chatters;
+                let allUsers = d.vips.concat(d.moderators).concat(d.staff).concat(d.admins).concat(d.gloabl_mods).concat(d.viewers);
+
+                for (let i = 0; i < allUsers.length; i++) {
+                    if (allUsers[i] == message.substring(12)) {
+                        client.action(channel, GlobalQueue.addChallenge(user["display-name"], allUsers[i]));
+
+                        GlobalQueue.timeouts.push(setTimeout(() => {
+                            let index = 0;
+                            for (let j = 0; j < GlobalQueue.challenges.length; j++) {
+                                if (GlobalQueue.challenges[j][0] == user["display-name"] && GlobalQueue.challenges[j][1] == allUsers[i]) {
+                                    let r = GlobalQueue.Queue.challenges.splice(j, 1);
+
+                                    GlobalQueue.timeouts.splice(j, 1);
+
+                                }
+                            }
+                        }, GlobalQueue.timeoutLength));
+
+                        break;
+                    }
+                }
+            });
+
+        });
+    }
+});
